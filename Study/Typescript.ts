8a1428c32b6b29ef4a398d9a9091951ce773fed6
Mohamed Yahia <97320765+0xYahia@users.noreply.github.com>
2023-06-28 21:37:43 +0300

    Object types

--- Study/Typescript.ts
+++ Study/Typescript.ts
@@ -101,3 +101,39 @@ const input2 = document.getElementById("num2")! as HTMLInputElement;
 // but in var or let variable when we assign value in this variables IDE write type of this variables implicitly
 //! -------------------------------------------------------------------------------------------------------------------------------------------------------------//
 //! 16- Object Types:
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+  const person = {
+  name: "Yahia",
+  age: 25,
+};
+
+console.log(person.name);
+
+// Of course object types can also be created for nested objects.
+
+//! Let's say you have this JavaScript object:
+
+const product = {
+  id: 'abc1',
+  price: 12.99,
+  tags: ['great-offer', 'hot-and-new'],
+  details: {
+    title: 'Red Carpet',
+    description: 'A great carpet - almost brand-new!'
+  }
+}
+// This would be the type of such an object:
+
+{
+  id: string;
+  price: number;
+  tags: string[];
+  details: {
+    title: string;
+    description: string;
+  }
+}
+// So you have an object type in an object type so to say.
