1f7fdd2d321ea91d67b452bd351a1b4c58730191
Mohamed Yahia <97320765+0xYahia@users.noreply.github.com>
2023-09-23 06:53:14 +0300

    containerizing an application

---
+++ Study/Docker/test/hello.py
@@ -0,0 +1,13 @@
+from flask import Flask;
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Flask Hello, World!'
+
+@app.route('/aboyahia')
+def test():
+    return 'A7la mesa on you from aboyahia! :)'
+
+if __name__ == '__main__':
+    app.run(debug=False,host='0.0.0.0',port=5000)
