f4c282301c1940f51180e6c0e7fc448fab42a2c3
Mohamed Yahia <97320765+0xYahia@users.noreply.github.com>
2024-01-27 04:50:26 +0200

    update

--- Study/Max Courses/Angular/recipe-project/src/app/app.module.ts
+++ Study/Max Courses/Angular/recipe-project/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import {NgOptimizedImage} from "@angular/common";
+import {DropdownDirective} from "./shared/dropdown.directive";
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import {NgOptimizedImage} from "@angular/common";
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent
+    ShoppingEditComponent,
+    DropdownDirective,
   ],
   imports: [
     BrowserModule,
