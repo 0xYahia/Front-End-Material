fb7d8afebbb6c4f35939277ddd457b5272523f39
Mohamed Yahia <97320765+0xYahia@users.noreply.github.com>
2024-01-30 07:55:28 +0200

    update

--- Study/Max Courses/Angular/services-start/src/app/account/account.component.ts
+++ Study/Max Courses/Angular/services-start/src/app/account/account.component.ts
@@ -6,17 +6,16 @@ import {AccountsService} from "../accounts.service";
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
-  providers:[LoggingService]
+  providers:[]
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
 
-  constructor(private LoggingService: LoggingService,
+  constructor(// private LoggingService: LoggingService,
               private accountsService: AccountsService) {}
 
   onSetTo(status: string) {
     this.accountsService.updateAccount(this.id, status);
-    this.LoggingService.logStatusChange(status)
   }
 }
--- Study/Max Courses/Angular/services-start/src/app/new-account/new-account.component.ts
+++ Study/Max Courses/Angular/services-start/src/app/new-account/new-account.component.ts
@@ -6,15 +6,15 @@ import {AccountsService} from "../accounts.service";
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers:[LoggingService]
+  providers:[]
 })
 export class NewAccountComponent {
 
-  constructor(private loggingService: LoggingService,
+  constructor(//private loggingService: LoggingService,
               private accountsService: AccountsService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus);
-    this.loggingService.logStatusChange((accountStatus));
+    // this.loggingService.logStatusChange((accountStatus));
   }
 }
