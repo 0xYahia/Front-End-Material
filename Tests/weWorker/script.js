26131a1f6edbaaae09e5384e143652f2ee48f2d8
Mohamed Yahia <97320765+0xYahia@users.noreply.github.com>
2024-02-04 17:33:54 +0200

    web worker

--- Tests/weWorker/script.js
+++ Tests/weWorker/script.js
@@ -4,8 +4,10 @@ const sumButton = document.getElementById("sumButton");
 const bgButton = document.getElementById("bgButton");
 
 sumButton.addEventListener("click", (event) => {
+  // let sum = 0;
+  // for (let i = 0; i < 10000000000; i++) sum += i;
   worker.postMessage("hello worker");
-  // alert(`The final sum is: ${sum}`);
+  alert(`The final sum is: ${sum}`);
 });
 
 worker.onmessage = function (message) {
