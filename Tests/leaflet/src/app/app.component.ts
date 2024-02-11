504116f40fc2fcf97412c445dedfa7d177c31364
Mohamed Yahia <97320765+0xYahia@users.noreply.github.com>
2024-02-11 07:55:55 +0200

    .

--- Tests/leaflet/src/app/app.component.ts
+++ Tests/leaflet/src/app/app.component.ts
@@ -1,6 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import * as L from 'leaflet'
+import {pointJson} from "./data/point";
+import {lineJson} from "./data/line";
+import {rectangleJson} from "./data/rectangle";
+import {polygonJson} from "./data/polygon";
+import {Layer} from "leaflet";
 
 @Component({
   selector: 'app-root',
@@ -21,19 +26,19 @@ export class AppComponent implements OnInit{
       osm.addTo(this.map);
 
       //! World Street Map
-      // var Esri_WorldStreetMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
-      //   attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
-      // });
-      //
-      // Esri_WorldStreetMap.addTo(this.map)
+      var Esri_WorldStreetMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+      });
+
+      Esri_WorldStreetMap.addTo(this.map)
 
       //! Dark map
-      // var DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-      //   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-      //   subdomains: 'abcd',
-      //   maxZoom: 20
-      // });
-      // DarkMatter.addTo(this.map)
+      let DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: 'abcd',
+        maxZoom: 20
+      });
+      DarkMatter.addTo(this.map)
 
       //! google map
       let googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
@@ -56,8 +61,45 @@ export class AppComponent implements OnInit{
         singleMarker.addTo(this.map)
         let popup = singleMarker.bindPopup(`This is Cairo , ${singleMarker.getLatLng()}  <br> I live in Cairo`).openPopup()
 
+      let secondMarker =  L.marker([30.033333, 31.433334], {icon: myIcon, draggable: true})
+
       console.log(singleMarker.toGeoJSON())
 
+      let baseMaps = {
+        "OSM": osm,
+        "World street map ": Esri_WorldStreetMap,
+        "Dark": DarkMatter,
+        "Google Street": googleStreets
+      };
 
+      let overlayMaps = {
+        "First Marker": singleMarker,
+        "Second Marker": secondMarker
+      };
+
+      // L.control.layers(baseMaps, overlayMaps, {collapsed: false}).addTo(this.map)
+      L.control.layers(baseMaps, overlayMaps).addTo(this.map)
+
+      //! Remove
+      this.map.removeLayer(osm)
+      this.map.removeLayer(singleMarker)
+
+      //! GeoJson
+      // @ts-ignore
+      L.geoJson(pointJson, {
+        onEachFeature: function (feature, layer) {
+          layer.bindPopup('<b>Name: </b>' + feature.properties.name)
+        }
+      }).addTo(this.map)
+      // @ts-ignore
+      L.geoJson(lineJson).addTo(this.map)
+      // @ts-ignore
+      L.geoJson(rectangleJson).addTo(this.map)
+      // @ts-ignore
+      L.geoJson(polygonJson, {
+        onEachFeature: function (feature, layer) {
+          layer.bindPopup('<b>Name: </b>' + feature.properties.name)
+        }
+      }).addTo(this.map)
     }
 }
