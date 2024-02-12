a64313d063d6cd50ecb96c45a0452e2d38f2805c
Mohamed Yahia <97320765+0xYahia@users.noreply.github.com>
2024-02-13 00:49:30 +0200

    .

--- Tests/leaflet/src/app/data/point.ts
+++ Tests/leaflet/src/app/data/point.ts
@@ -1,1 +1,65 @@
-export let pointJson = {"type":"FeatureCollection","features":[{"type":"Feature","properties":{"name":"point"},"geometry":{"coordinates":[31.10565454529697,30.0981764019553],"type":"Point"}},{"type":"Feature","properties":{"name":"point"},"geometry":{"coordinates":[31.13787838977754,30.057970575153675],"type":"Point"}},{"type":"Feature","properties":{"name":"point"},"geometry":{"coordinates":[31.181341426353185,30.100120646228078],"type":"Point"}},{"type":"Feature","properties":{"name":"point"},"geometry":{"coordinates":[31.246512927836818,30.036570535352936],"type":"Point"}},{"type":"Feature","properties":{"name":"point"},"geometry":{"coordinates":[31.32644946365076,30.024941900219673],"type":"Point"}},{"type":"Feature","properties":{"name":"point"},"geometry":{"coordinates":[31.42411723002948,30.011401987475395],"type":"Point"}}]}
+export let pointJson = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "point"
+      },
+      "geometry": {
+        "coordinates": [31.10565454529697,30.0981764019553],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "point"
+      },
+      "geometry": {
+        "coordinates": [31.13787838977754,30.057970575153675],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "point"
+      },
+      "geometry": {
+        "coordinates": [31.181341426353185,30.100120646228078],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "point"
+      },
+      "geometry": {
+        "coordinates": [31.246512927836818,30.036570535352936],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "point"
+      },
+      "geometry": {
+        "coordinates": [31.32644946365076,30.024941900219673],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "point"
+      },
+      "geometry": {
+        "coordinates": [31.42411723002948,30.011401987475395],
+        "type": "Point"
+      }
+    }
+  ]
+}
--- Tests/leaflet/src/app/data/rectangle.ts
+++ Tests/leaflet/src/app/data/rectangle.ts
@@ -1,1 +1,24 @@
-export let rectangleJson = {"type":"FeatureCollection","features":[{"type":"Feature","properties":{"name":"rectangle"},"geometry":{"coordinates":[[[31.166854034306937,30.0801021611195],[31.166854034306937,30.004346892921333],[31.310590694834076,30.004346892921333],[31.310590694834076,30.0801021611195],[31.166854034306937,30.0801021611195]]],"type":"Polygon"},"id":0}]}
+export let rectangleJson = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "rectangle"
+      },
+      "geometry": {
+        "coordinates": [
+          [
+            [31.166854034306937,30.0801021611195],
+            [31.166854034306937,30.004346892921333],
+            [31.310590694834076,30.004346892921333],
+            [31.310590694834076,30.0801021611195],
+            [31.166854034306937,30.0801021611195]
+          ]
+        ],
+        "type": "Polygon"
+      },
+      "id": 0
+    }
+  ]
+}
