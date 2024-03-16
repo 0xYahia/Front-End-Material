25693872d076af0fac86b8e8910fb90c6a52531b
Mohamed Yahia <97320765+0xYahia@users.noreply.github.com>
2024-03-16 02:15:46 +0200

    assignment for reactive form

--- Study/Max Courses/Angular/13-forms-reactive-assignment-solution/src/app/app.component.ts
+++ Study/Max Courses/Angular/13-forms-reactive-assignment-solution/src/app/app.component.ts
@@ -9,18 +9,20 @@ import { CustomValidators } from './custom-validators';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  projectForm: FormGroup;
 
+  projectForm: FormGroup;
   ngOnInit() {
     this.projectForm = new FormGroup({
-      'projectName': new FormControl(
-        null,
-        [Validators.required, CustomValidators.invalidProjectName],
-        CustomValidators.asyncInvalidProjectName
-      ),
+      'projectName': new FormControl(null, [Validators.required, CustomValidators.invalidProjectName], CustomValidators.asyncInvalidProjectName),
       'email': new FormControl(null, [Validators.required, Validators.email]),
       'projectStatus': new FormControl('critical')
     });
+
+    this.projectForm.statusChanges.subscribe(
+      (status) => console.log(status)
+    );
+
+
   }
 
   onSaveProject() {
