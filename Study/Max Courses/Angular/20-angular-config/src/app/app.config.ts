e4ab5915fefb3065b213677ba410c268a2d0ec22
Mohamed Yahia <97320765+0xYahia@users.noreply.github.com>
2024-08-18 15:57:04 +0300

    multiple projects in one folder

--- Study/Max Courses/Angular/20-angular-config/src/app/app.config.ts
+++ Study/Max Courses/Angular/20-angular-config/src/app/app.config.ts
@@ -3,7 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"deploy-angular-app-95254","appId":"1:881356472231:web:6f56b6ac5d865a82cac082","storageBucket":"deploy-angular-app-95254.appspot.com","apiKey":"AIzaSyAolwws-GLiw4klLNmteVFW0-4sgVzppxQ","authDomain":"deploy-angular-app-95254.firebaseapp.com","messagingSenderId":"881356472231","measurementId":"G-6QP9P9B66D"})), provideDatabase(() => getDatabase())]
 };
