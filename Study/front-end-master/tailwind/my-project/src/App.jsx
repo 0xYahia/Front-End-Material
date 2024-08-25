c1bc3da80e81aba4c6f84b5b4fa4d0c387a7b0c0
Mohamed Yahia <97320765+0xYahia@users.noreply.github.com>
2024-08-25 05:01:51 +0300

    Update App.jsx

--- Study/front-end-master/tailwind/my-project/src/App.jsx
+++ Study/front-end-master/tailwind/my-project/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-mandy-4">
+      <h1 className="text-3xl text font-bold underline text-mandy-4">
         Hello world!
       </h1>
     </>
