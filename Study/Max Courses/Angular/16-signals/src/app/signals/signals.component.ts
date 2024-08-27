c3ccb3ed35dc6f88906f90d7ac010a615def8783
Mohamed Yahia <97320765+0xYahia@users.noreply.github.com>
2024-08-27 19:46:42 +0300

    update

--- Study/Max Courses/Angular/16-signals/src/app/signals/signals.component.ts
+++ Study/Max Courses/Angular/16-signals/src/app/signals/signals.component.ts
@@ -14,24 +14,24 @@ export class SignalsComponent {
   doubleCounter: Signal<number> = computed(() => this.counter() * 2);
 
   constructor() {
-    effect(() => console.log(this.counter() * 5))
+    effect(() => console.log(this.counter() * 5));
   }
 
-  increment() {
+  increment(): void {
     console.log('INCREMENT');
     // this.counter.update(oldCounter => oldCounter + 1);
     //! Update in old array
-    this.counter.set(this.counter() + 1)
+    this.counter.set(this.counter() + 1);
     // this.actions.push('INCREMENT');
-    this.actions.mutate(oldActions => oldActions.push('INCREMENT'))
+    this.actions.mutate(oldActions => oldActions.push('INCREMENT'));
   }
 
-  decrement() {
+  decrement(): void {
     if (this.counter() === 0) return;
     console.log('DECREMENT');
     this.counter.update((oldCounter) => oldCounter - 1);
     // this.actions.push('DECREMENT');
     //! this act as mutate because i edit in current array
-    this.actions.update(oldActions => [...oldActions, 'DECREMENT'])
+    this.actions.update(oldActions => [...oldActions, 'DECREMENT']);
   }
 }
