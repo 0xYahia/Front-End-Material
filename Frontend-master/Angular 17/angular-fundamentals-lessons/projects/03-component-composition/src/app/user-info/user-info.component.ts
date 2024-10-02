e7e1651ec1e20c0f10de6845b5d8a5228855fb66
Mohamed Yahia <97320765+0xYahia@users.noreply.github.com>
2024-10-02 16:59:48 +0300

    Dependency Injection

--- Frontend-master/Angular 17/angular-fundamentals-lessons/projects/03-component-composition/src/app/user-info/user-info.component.ts
+++ Frontend-master/Angular 17/angular-fundamentals-lessons/projects/03-component-composition/src/app/user-info/user-info.component.ts
@@ -1,17 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { User } from '../../../../11-dependency-injection/src/app/data';
 
 @Component({
   selector: 'app-user-info',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      user-info works!
-    </p>
+    <p>{{user.id}}</p>
+    <p>{{user.name}}</p>
   `,
   styles: ``
 })
 export class UserInfoComponent {
+
+  @Input() user: User = {
+    id: -1,
+    name: 'test',
+    username: 'test',
+    email: 'test',
+    address: {
+      street: 'test',
+      suite: 'test',
+      city: 'test',
+      zipcode: 'test',
+      geo: {
+        lat: '156456',
+        lng: '16516'
+      },
+    },
+    phone: '1561',
+    website: 'test',
+    company: {
+      name: 'test',
+      catchPhrase: 'test',
+      bs: 'test'
+    }
+
+  }
 
 }
