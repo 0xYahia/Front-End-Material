b2a53118f9b2e96dbcec0e143b7acbdc01ff82e6
Mohamed Yahia <97320765+0xYahia@users.noreply.github.com>
2024-10-02 14:32:57 +0300

    dynamic routing

--- Frontend-master/Angular 17/angular-fundamentals-lessons/projects/08-routing-recap/src/app/app.component.ts
+++ Frontend-master/Angular 17/angular-fundamentals-lessons/projects/08-routing-recap/src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: ` <h1>Welcome to {{ title }}!</h1> `,
+  imports: [RouterOutlet, RouterLink],
+  template: `
+  <h1>Welcome to {{ title }}!</h1>
+  @for (productTitle of productTitles; track productTitle) {
+    <p><a [routerLink]="['details', $index]" >{{productTitle}} | {{$index}}</a></p>
+
+    <router-outlet />
+  }
+
+  `,
 })
 export class AppComponent {
   title = '08-routing-recap';
