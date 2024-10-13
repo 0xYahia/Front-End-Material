71e8bf93ce0e8ed7400a45aa673fa14b34d45333
Mohamed Yahia <97320765+0xYahia@users.noreply.github.com>
2024-10-13 19:29:19 +0300

    dynamic component

--- Study/React/level1-p4-advanced-techniques/src/App.js
+++ Study/React/level1-p4-advanced-techniques/src/App.js
@@ -4,23 +4,39 @@ import ShapeOne from './components/ShapeOne'
 import ShapeTwo from './components/ShapeTwo'
 
 const App = () => {
+  // const userData = [
+  //   { id: 1, name: 'yahia', email: 'yahia@gmail.coom', age: 27 },
+  //   { id: 2, name: 'ahmed', email: 'ahmed@gmail.coom', age: 29 },
+  //   { id: 3, name: 'wafaa', email: 'Sayed@gmail.coom', age: 30 },
+  //   { id: 4, name: 'safaa', email: 'Mohamed@gmail.coom', age: 31 },
+  // ]
+
   const userData = [
-    { id: 1, name: 'yahia', email: 'yahia@gmail.coom', age: 27 },
-    { id: 2, name: 'ahmed', email: 'ahmed@gmail.coom', age: 29 },
-    { id: 3, name: 'wafaa', email: 'Sayed@gmail.coom', age: 30 },
-    { id: 4, name: 'safaa', email: 'Mohamed@gmail.coom', age: 31 },
+    {
+      componentName: ShapeOne,
+      data: { id: 1, name: 'yahia', email: 'yahia@gmail.coom', age: 27 },
+    },
+    {
+      componentName: ShapeTwo,
+      data: { id: 4, name: 'safaa', email: 'Mohamed@gmail.coom', age: 31 },
+    },
   ]
+
+  const child = userData.map((el) => {
+    const ComponentName = el.componentName
+    return <ComponentName item={el.data} key={el.data.id} />
+  })
 
   return (
     <div className='App'>
-      <List items={userData}>
-        <ShapeOne />
+      {/* <List items={userData}>
+         <ShapeOne />
+       </List>
+       <List items={userData}>
+         <ShapeTwo />
       </List>
-      <List items={userData}>
-        <ShapeTwo />
-      </List>
-
-      <ShapeOne item={userData[0]} />
+      <ShapeOne item={userData[0]} /> */}
+      {child}
     </div>
   )
 }
